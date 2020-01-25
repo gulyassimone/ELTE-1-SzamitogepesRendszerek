1832b3de4116a4683cc42fa81e5f9708cf76462f
Gulyás Simone <gulyassimone@gmail.com>
2020-01-25 20:56:53 +0100

    allomany

---
+++ Power_Shell_5_feladat_foreach_foreach_substring.ps1
@@ -0,0 +1,6 @@
+﻿Write-Host "Minden sor 2-4 karaktere "
+
+foreach($i in Get-Content $args[0])
+{
+    $i.Substring(1,3)
+} 
---
+++ Power_Shell_zh_2_maximum_tetel.ps1
@@ -0,0 +1,18 @@
+﻿[int]$max=$args[0]
+if($args.Count -gt 0)
+{
+    foreach ($i in $args)
+    {
+        if($max -lt $i)
+        {
+          $max=$i
+        }
+     }
+     Write-Host "A legnagyobb ertek: $max" -NoNewline
+}
+else
+{
+     Write-Host "A legnagyobb ertek:" -NoNewline
+}
+    
+
